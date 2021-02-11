e15af09482165cb52729d83ea7dfa0ad38571be4
Afranklin6262 <73451476+Afranklin6262@users.noreply.github.com>
2021-02-11 10:08:35 -0600

    Update recipes.sql

--- recipes.sql
+++ recipes.sql
@@ -23,17 +23,26 @@ create table RecipeIngredient (recipe_id INT NOT NULL, ingredient_id INT NOT NUL
 
 INSERT INTO Measure (name) VALUES('CUP'), ('TEASPOON'), ('TABLESPOON'), ('WHOLE');
 
-INSERT INTO Ingredient (name) VALUES('egg'), ('salt'), ('sugar'), ('chocolate'), ('vanilla extract'), ('flour');
+INSERT INTO Ingredient (name) VALUES('egg'), ('salt'), ('sugar'), ('chocolate'), ('vanilla extract'), ('flour'), ('milk'), ('chicken');
 
 INSERT INTO Recipe (name, description, instructions) VALUES('Boiled Egg', 'A single boiled egg', 'Add egg to cold water. Bring water to boil. Cook.');
 INSERT INTO Recipe (name, description, instructions) VALUES('Chocolate Cake', 'Yummy cake', 'Add eggs, flour, chocolate to pan. Bake at 350 for 1 hour');
+INSERT INTO Recipe (name, description, instructions) VALUES('Crepes', 'really thin yummy pancakes', 'Mix flour, milk, egg, water, sugar, and salt and fry for 1 minute each side');
+INSERT INTO Recipe (name, description, instructions) VALUES('Rotisserie Chicken', 'Chicken cooked in a rotisserie', 'Place chicken in rotisserie until thickest part reaches 165');
+INSERT INTO Recipe (name, description, instructions) VALUES('Sunny Side Up', 'type of fried egg', 'Crack egg in pan and cover for 1 minute until cooked with runny yolk');
 
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount) VALUES (1, 1, NULL, 1);
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 1, NULL, 3);
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 2, 2, 1);
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 3, 1, 2);
 INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (2, 4, 1, 1);
-
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (3, 1, NULL, 2);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (3, 2, 2, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (3, 3, 3, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (3, 6, 1, 2);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (3, 7, 1, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (4, 8, NULL, 1);
+INSERT INTO RecipeIngredient (recipe_id, ingredient_id, measure_id, amount)  VALUES (5, 1, NULL, 1);
 /* The SELECT statement is the most common way to read data from the database.  This is the "Read" in CRUD */
 
 /*get a list of all recipes */ 
